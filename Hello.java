1645a7812f5d091f94150f8b213c2d6d78af299f
root <root@ansibleserver.vvnepmb1txjupatwsth0w4ucid.bx.internal.cloudapp.net>
2018-10-31 09:25:28 +0000

    1a

--- Hello.java
+++ Hello.java
@@ -1,11 +1,8 @@
 public class Hello{
 
 	public static void main(String[] args){
-<<<<<<< HEAD
 		for(int i=1;i<=10;i++){
-=======
 		for(int i=1;i<=9;i++){
->>>>>>> 285b08abca44b84f86bd64c7a261ac9dd60a7a80
 			System.out.println("Hello WOrld .. " +i);
 
 	}
